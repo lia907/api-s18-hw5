b72626aa86c969523462996b781f97c80cf6c8d5
Soojin <soo.kim@ucdenver.edu>
2018-04-11 18:07:41 -0600

    added directories and store

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import MovieHeader from './components/movieheader';
 import MovieList from './components/movielist';
 import Movie from './components/movie';
+import Authentication from './components/authentication';
+import { Provider } from 'react-redux';
 import {HashRouter,Route} from 'react-router-dom';
+import store from './stores/store';
 
 //add routing configuration
 
@@ -18,14 +21,18 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <Provider store={store}>
                 <HashRouter>
                     <div>
-                        <MovieHeader subTitle={this.state.title}/>
-                        <Route exact path="/" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
-                        <Route path="/movielist" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
-                        <Route path="/movie" render={()=><Movie onTitleChange={this.handleOnTitleChange}/>}/>
+                        {/*<MovieHeader subTitle={this.state.title}/>*/}
+                        {/*<Route exact path="/" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>*/}
+                        {/*<Route path="/movielist" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>*/}
+                        {/*<Route path="/movie" render={()=><Movie onTitleChange={this.handleOnTitleChange}/>}/>*/}
+                        <MovieHeader />
+                        <Route exact path="/" render={()=> <MovieList />}
                     </div>
                 </HashRouter>
+                </Provider>
             </div>
         );
     }
